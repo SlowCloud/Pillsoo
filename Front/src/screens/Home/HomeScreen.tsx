6e72876c6b20d7e898a4f4405a129a122b686f37
구현우 <kjklovekhw@gmail.com>
2024-09-11 09:47:23 +0900

    [FE] feat : 알람 기능 구현중

--- Front/src/screens/Home/HomeScreen.tsx
+++ Front/src/screens/Home/HomeScreen.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Kit from '../../components/Home/Kit';
 import Header from '../../components/common/Header';
+import {useNavigation} from '@react-navigation/native';
+
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <Header />
       <View style={styles.container}>
         <Text style={{fontSize: 25}}>현우님 안녕하세요 !</Text>
+      </View>
+      <View style={styles.alarm}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('Home', {
+              screen: 'Alarm',
+            })
+          }>
+          <Text>알람 설정 !</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.kit}>
         <Kit />
@@ -26,6 +40,11 @@ const styles = StyleSheet.create({
   kit: {
     flex: 1,
     alignItems: 'center',
+  },
+  alarm: {
+    flex: 0.2,
+    alignItems: 'flex-end',
+    paddingRight: 30,
   },
 });
 
