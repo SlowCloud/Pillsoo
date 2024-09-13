5813bde6bea92d4671060e063791fb8b55e1994a
jhpark <brianwogus@naver.com>
2024-09-13 14:43:55 +0900

    [BE] feat : 내 리뷰 보러가기 jwt 수정완료

--- PillSoo/src/main/java/org/sos/pillsoo/supplement/controller/MyPageController.java
+++ PillSoo/src/main/java/org/sos/pillsoo/supplement/controller/MyPageController.java
@@ -2,7 +2,10 @@ package org.sos.pillsoo.supplement.controller;
 
 import org.sos.pillsoo.supplement.dto.ReviewDto;
 import org.sos.pillsoo.supplement.service.MyPageService;
+import org.sos.pillsoo.auth.dto.CustomUserDetails;  // JWT에서 userSeq 가져오기
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,9 +17,17 @@ public class MyPageController {
     @Autowired
     private MyPageService myPageService;
 
+    // JWT에서 userSeq 추출하는 메서드
+    private int getUserSeqFromJWT() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        return userDetails.getUserSeq();
+    }
+
     // 내 리뷰 조회 (마이페이지)
     @GetMapping
-    public List<ReviewDto> getMyReviews(@RequestParam int userSeq) {
+    public List<ReviewDto> getMyReviews() {
+        int userSeq = getUserSeqFromJWT();  // JWT에서 userSeq 추출
         return myPageService.getMyReviews(userSeq);
     }
 }
