24206798fc0a2b93ab7d51c117c219eb0bb55c0e
서정운 <sju9417@gmail.com>
2024-09-20 11:31:23 +0900

    [BE] chore: import 최적화

--- PillSoo/src/main/java/org/sos/pillsoo/jwt/RefreshTokenFilter.java
+++ PillSoo/src/main/java/org/sos/pillsoo/jwt/RefreshTokenFilter.java
@@ -1,6 +1,7 @@
 package org.sos.pillsoo.jwt;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,10 +12,7 @@ import org.sos.pillsoo.auth.repository.RefreshRepository;
 import org.sos.pillsoo.auth.service.CustomUserDetailsService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
--- PillSoo/src/main/java/org/sos/pillsoo/mykit/service/AlarmService.java
+++ PillSoo/src/main/java/org/sos/pillsoo/mykit/service/AlarmService.java
@@ -5,7 +5,6 @@ import org.sos.pillsoo.mykit.entity.Alarm;
 import org.sos.pillsoo.mykit.entity.Cabinet;
 import org.sos.pillsoo.mykit.repository.AlarmRepository;
 import org.sos.pillsoo.mykit.repository.CabinetRepository;
-import org.sos.pillsoo.supplement.entity.Supplement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
