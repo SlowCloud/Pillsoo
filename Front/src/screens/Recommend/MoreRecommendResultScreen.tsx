1368728c4daf09b4425d83a9f888a373d51d572d
구현우 <kjklovekhw@gmail.com>
2024-09-25 16:47:16 +0900

    [FE] feat : merge

--- Front/src/screens/Recommend/MoreRecommendResultScreen.tsx
+++ Front/src/screens/Recommend/MoreRecommendResultScreen.tsx
@@ -1,10 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import axios from 'axios';
 import {API_URL} from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
+type Supplement = {
+  functionality: string;
+  image_url: string;
+  pill_name: string;
+  supplementSeq: number;
+};
 
 type MoreRecommendResultProps = {
   route: {
@@ -17,9 +30,9 @@ type MoreRecommendResultProps = {
 const MoreRecommendResultScreen: React.FC<MoreRecommendResultProps> = ({
   route,
 }) => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   const {inputText} = route.params;
-  const [recommendations, setRecommendations] = useState<any[]>([]);
+  const [recommendations, setRecommendations] = useState<Supplement[]>([]);
   const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
@@ -33,7 +46,6 @@ const MoreRecommendResultScreen: React.FC<MoreRecommendResultProps> = ({
 
   useEffect(() => {
     const fetchRecommendations = async () => {
-      console.log(inputText);
       try {
         const response = await axios.get(`${API_URL}/api/v1/recommend/survey`, {
           params: {client_text: inputText},
@@ -41,66 +53,65 @@ const MoreRecommendResultScreen: React.FC<MoreRecommendResultProps> = ({
             Authorization: `Bearer ${token}`,
           },
         });
-
-        console.log('res', response);
         setRecommendations(response.data);
       } catch (err) {
-        console.log('err', err);
+        console.log(err);
       }
     };
 
     if (token) {
       fetchRecommendations();
     }
-  }, [token]);
+  }, [inputText, token]);
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.resultText}>당신의 건강 상태: {inputText}</Text>
       <Text style={styles.recommendationTitle}>추천 영양제:</Text>
       {recommendations.map(item => (
-        <View key={item.supplementSeq} style={styles.recommendationContainer}>
-          <Text style={styles.recommendationText}>{item.pill_name}</Text>
-        </View>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('Detail', {id: item.supplementSeq})
+          }>
+          <View key={item.supplementSeq} style={styles.recommendationContainer}>
+            <Image source={{uri: item.image_url}} style={styles.image} />
+            <Text style={styles.pillName}>{item.pill_name}</Text>
+            <Text>{item.functionality}</Text>
+          </View>
+        </TouchableOpacity>
       ))}
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     padding: 20,
   },
   resultText: {
     fontSize: 20,
-    margin: 10,
+    marginVertical: 10,
     color: 'black',
   },
   recommendationTitle: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginTop: 20,
+    marginVertical: 20,
   },
   recommendationContainer: {
-    marginVertical: 10,
+    marginBottom: 20,
     alignItems: 'center',
   },
-  recommendationText: {
+  pillName: {
     fontSize: 16,
+    fontWeight: 'bold',
     marginVertical: 5,
     color: 'black',
   },
-  recommendationImage: {
+  image: {
     width: 100,
     height: 100,
-    marginTop: 10,
-  },
-  noImageText: {
-    fontSize: 14,
-    color: 'gray',
+    marginBottom: 10,
   },
 });
 
