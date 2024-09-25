03d04cb25c3fea1bb66482e2c2c6864bbd4ce7d2
jhpark <brianwogus@naver.com>
2024-09-25 14:49:45 +0900

    [FE] feat : text 기반 영양제 추천

--- Front/src/screens/Recommend/MoreRecommendResultScreen.tsx
+++ Front/src/screens/Recommend/MoreRecommendResultScreen.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import axios from 'axios';
 import {API_URL} from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
 
 type MoreRecommendResultProps = {
   route: {
@@ -15,6 +17,7 @@ type MoreRecommendResultProps = {
 const MoreRecommendResultScreen: React.FC<MoreRecommendResultProps> = ({
   route,
 }) => {
+  const navigation = useNavigation()
   const {inputText} = route.params;
   const [recommendations, setRecommendations] = useState<any[]>([]);
   const [token, setToken] = useState<string | null>(null);
@@ -30,6 +33,7 @@ const MoreRecommendResultScreen: React.FC<MoreRecommendResultProps> = ({
 
   useEffect(() => {
     const fetchRecommendations = async () => {
+      console.log(inputText);
       try {
         const response = await axios.get(`${API_URL}/api/v1/recommend/survey`, {
           params: {client_text: inputText},
@@ -37,27 +41,27 @@ const MoreRecommendResultScreen: React.FC<MoreRecommendResultProps> = ({
             Authorization: `Bearer ${token}`,
           },
         });
-        console.log('hi');
-        console.log(response);
+
+        console.log('res', response);
         setRecommendations(response.data);
       } catch (err) {
-        console.log(err);
+        console.log('err', err);
       }
     };
 
     if (token) {
       fetchRecommendations();
     }
-  }, [inputText]);
+  }, [token]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.resultText}>당신의 건강 상태: {inputText}</Text>
       <Text style={styles.recommendationTitle}>추천 영양제:</Text>
       {recommendations.map(item => (
-        <Text key={item.id} style={styles.recommendationText}>
-          {item.name}: {item.description}
-        </Text>
+        <View key={item.supplementSeq} style={styles.recommendationContainer}>
+          <Text style={styles.recommendationText}>{item.pill_name}</Text>
+        </View>
       ))}
     </View>
   );
@@ -80,13 +84,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 20,
   },
+  recommendationContainer: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
   recommendationText: {
     fontSize: 16,
     marginVertical: 5,
     color: 'black',
   },
-  errorText: {
-    color: 'red',
+  recommendationImage: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+  },
+  noImageText: {
+    fontSize: 14,
+    color: 'gray',
   },
 });
 
