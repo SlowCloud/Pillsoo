98ab141593f4e3bb436b733504bb827a546a38c8
김구태 <gootae0220@naver.com>
2024-09-25 12:05:55 +0900

    [BE] feat : lazy loading 기능 추가

--- GT/Pillsoo/app/database.py
+++ GT/Pillsoo/app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 import redis
+from pymongo import MongoClient
+
 
 # MySQL 데이터베이스 URL 설정
 DATABASE_URL = "mysql+pymysql://root:ssafypasswordj11e205@j11e205.p.ssafy.io:30306/SOS"
@@ -15,6 +17,11 @@ try:
     r = redis.Redis(host='localhost', port=6379, db=0)
 except redis.exceptions.ConnectionError as e:
     r = None  # Redis가 사용 불가한 경우 None으로 설정
+
+# MongoDB 설정
+mongo_client = MongoClient("mongodb://localhost:27017/")
+mongo_db = mongo_client["sos_db"]  # MongoDB 데이터베이스
+mongo_collection = mongo_db["recommended"]  # MongoDB 컬렉션
 
 # DB 세션을 반환하는 의존성
 def get_db():
