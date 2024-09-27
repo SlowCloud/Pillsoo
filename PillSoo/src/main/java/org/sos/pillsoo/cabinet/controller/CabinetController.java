5881eb3a89551337291df350d1e9fd8e34bfecb0
이형준 <95joon@naver.com>
2024-09-27 11:46:05 +0900

    refator: cabinet controller api주소 변경 (/my-kit -> /cabinet)

--- PillSoo/src/main/java/org/sos/pillsoo/cabinet/controller/CabinetController.java
+++ PillSoo/src/main/java/org/sos/pillsoo/cabinet/controller/CabinetController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/my-kifghfhgfhfgt")
+@RequestMapping("/api/v1/cabinet")
 public class CabinetController {
 
     @Autowired
