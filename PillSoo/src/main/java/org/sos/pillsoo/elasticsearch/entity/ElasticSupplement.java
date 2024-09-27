030916059dc80deea1345323f244e704308e4c6e
서정운 <sju9417@gmail.com>
2024-09-27 14:59:57 +0900

    [BE] chore: 필드 순서 조정

--- PillSoo/src/main/java/org/sos/pillsoo/elasticsearch/entity/ElasticSupplement.java
+++ PillSoo/src/main/java/org/sos/pillsoo/elasticsearch/entity/ElasticSupplement.java
@@ -16,20 +16,29 @@ public class ElasticSupplement {
     @Id
     String id;
 
-    @Field(name = "appearance")
-    private String appearance;
+    @Field(name = "supplementSeq")
+    long supplementSeq;
 
     @Field(name = "click_count")
     private long clickCount;
 
-    @Field(name = "dose_amount")
-    private String doseAmount;
-
-    @Field(name = "dose_guide")
-    private String doseGuide;
+    @Field(name = "pill_name")
+    private String pillName;
 
     @Field(name = "expiration_date")
     private String expirationDate;
+
+    @Field(name = "appearance")
+    private String appearance;
+
+    @Field(name = "dose_amount")
+    private String doseAmount;
+
+    @Field(name = "storage_method")
+    private String storageMethod;
+
+    @Field(name = "dose_guide")
+    private String doseGuide;
 
     @Field(name = "functionality")
     private String functionality;
@@ -37,15 +46,6 @@ public class ElasticSupplement {
     @Field(name = "image_url")
     private String imageUrl;
 
-    @Field(name = "supplementSeq")
-    long supplementSeq;
-
-    @Field(name = "pill_name")
-    private String pillName;
-
     @Field(name = "preprocessed_text")
     private String preprocessedText;
-
-    @Field(name = "storage_method")
-    private String storageMethod;
 }
