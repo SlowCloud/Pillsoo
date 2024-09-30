888ab117a60fb6c37fc050b57bbc5cc02e38e852
서정운 <sju9417@gmail.com>
2024-09-30 10:50:15 +0900

    [BE] feat: DTO toString 추가

--- PillSoo/src/main/java/org/sos/pillsoo/alarm/dto/AlarmReqDto.java
+++ PillSoo/src/main/java/org/sos/pillsoo/alarm/dto/AlarmReqDto.java
@@ -2,11 +2,13 @@ package org.sos.pillsoo.alarm.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalTime;
 
 @Getter
 @Setter
+@ToString
 public class AlarmReqDto {
     private int supplementSeq; // 영양제 seq
     private LocalTime time;
