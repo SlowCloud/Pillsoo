fe2a0a9dcfb341d383eb9329f0d93629ccf78577
서정운 <sju9417@gmail.com>
2024-10-10 08:56:05 +0900

    [BE] chore: 파이썬 서버 DB 주소 분리

--- Back_Python/Pillsoo/app/database.py
+++ Back_Python/Pillsoo/app/database.py
@@ -5,27 +5,31 @@ from pymongo import MongoClient
 from elasticsearch import Elasticsearch
 
 
-# MySQL 데이터베이스 URL 설정
-DATABASE_URL = "mysql+pymysql://root:ssafypasswordj11e205@j11e205.p.ssafy.io:30306/SOS"
+# 데이터베이스 설정
+MYSQL_DATABASE_URL = "mysql+pymysql://root:ssafypasswordj11e205@j11e205.p.ssafy.io:30306/SOS"
+REDIS_HOST_URL = "j11e205.p.ssafy.io"
+REDIS_HOST_PORT = 30379
+MONGODB_DATABASE_URL = "mongodb://j11e205.p.ssafy.io:30017"
+ELASTICSEARCH_URL = "http://j11e205-elasticsearch.duckdns.org:80"
 
 # SQLAlchemy 엔진과 세션 설정
-engine = create_engine(DATABASE_URL)
+engine = create_engine(MYSQL_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
     
 # Redis 클라이언트 설정 (원격 서버로 변경)
 try:
-    r = redis.Redis(host='j11e205.p.ssafy.io', port=30379, db=0)
+    r = redis.Redis(host=REDIS_HOST_URL, port=REDIS_HOST_PORT, db=0)
 except redis.exceptions.ConnectionError as e:
     r = None  # Redis가 사용 불가한 경우 None으로 설정    
 
 # MongoDB 설정
-mongo_client = MongoClient("mongodb://j11e205.p.ssafy.io:30017/")
+mongo_client = MongoClient(MONGODB_DATABASE_URL)
 mongo_db = mongo_client["sos_db"]  # MongoDB 데이터베이스
 mongo_collection = mongo_db["recommended"]  # MongoDB 컬렉션
 
 # ElasticSearch 클라이언트 생성 (지정된 주소의 80번 포트로 지정)
-es = Elasticsearch(hosts=["http://j11e205-elasticsearch.duckdns.org:80/"])
+es = Elasticsearch(hosts=[ELASTICSEARCH_URL])
 
 # DB 세션을 반환하는 의존성
 def get_db():   
